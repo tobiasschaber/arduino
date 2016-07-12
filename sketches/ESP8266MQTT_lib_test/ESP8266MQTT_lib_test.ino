75f53b5aa3c394792234f2cae59aa4166ed6ffcf
Tobias Schaber <tobias.schaber@codecentric.de>
2016-07-12 08:08:13 +0200

    adjusted byte handling in mqtt library sketch

--- sketches/ESP8266MQTT_lib_test/ESP8266MQTT_lib_test.ino
+++ sketches/ESP8266MQTT_lib_test/ESP8266MQTT_lib_test.ino
@@ -1,6 +1,7 @@
+#include <ESP8266MQTT.h>
+
 
 #include "ESP8266.h"
-#include "ESP8266MQTT.h"
 
 // WLAN credentials
 #define wlanSSID        "codecentric"
@@ -21,6 +22,15 @@ ESP8266MQTT mqtt;
 void setup() {
   Serial.begin(115200);
 
+  connectWLAN();
+
+  delay(1000);
+
+}
+
+
+void connectWLAN() {
+
   if (wifi.setOprToStation()) {
     Serial.println("to station: OK");
   } else {
@@ -40,101 +50,110 @@ void setup() {
   } else {
     Serial.println("enable dhcp: ERROR");
   }
-
-  
+    
   if (wifi.joinAP(wlanSSID, wlanPass)) {
     Serial.println("join wlan: OK");
   } else {
     Serial.println("join wlan: ERROR");
   }
 
+  /* reset value of myip */
+  myip = "";
 
 
+  /* wait until we have an IP address */
+  for(int i=0; i<10; i++) {
+    
+    /* extract only the IP from the get-IP-message */
+    String ip = wifi.getLocalIP().c_str();
+    String ip2 = ip.substring(14);
+    
+    myip = ip2.substring(0, ip2.indexOf('"'));
+    
+    if(myip != "") {
+      Serial.println("Deine IP: ");
+      Serial.println(myip);
+      i=10;
+    }
+  
+    delay(2000);
+  }
 
+
+  
+  
 }
 
 /* ======================================================================================================================== */
 /* LOOP */
 /* ======================================================================================================================== */
 void loop() {
-
-  if(myip == "") {  
   
+  Serial.println("========================================== STARTE SCHREIBVORGANG");
+
+  mqttConnect("testid");
+
   
-    for(int i=0; i<10; i++) {
-      Serial.print(".");
-      
-      /* extract only the IP from the get-IP-message */
-      String ip = wifi.getLocalIP().c_str();
-      String ip2 = ip.substring(14);
-      
-      myip = ip2.substring(0, ip2.indexOf('"'));
-      
-      if(myip != "") {
-        Serial.println("Deine IP: ");
-        Serial.println(myip);
-        i=10;
-      }
-    
-      delay(2000);
-    }
+Serial.println("X");
+delay(5000);
+  //dada();
+Serial.println("Y");
+  
 
-  }
+  mqttDisconnect();
+
+Serial.println("Z");
+}
 
 
- delay(2000);
- Serial.println("========================================== STARTE SENDEVORGANG");
+/* =======================================================================
+ *  connect to MQTT -> create TCP connection, send CONNECT und wait for CONNACK message
+ * ======================================================================= */
+bool mqttConnect(char* id) {
 
+  bool exas = wifi.createTCP(mqttServerName, mqttServerPort);
 
- bool exas = wifi.createTCP(mqttServerName, mqttServerPort);
+  char* conMsg = reinterpret_cast<char*>(
+    mqtt.getConnectMessage("testid")
+  );
+  
 
- 
- 
- delay(3000);
- 
- 
- 
- char* conMsg = reinterpret_cast<char*>(
-      mqtt.getConnectMessage("testid")
- s   );
- 
- //char *hello = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n";
- //wifi.send((const uint8_t*)hello, strlen(hello));
+  /* send CONNECT */
+  wifi.send( mqtt.getConnectMessage("testid"), strlen(conMsg));
+  
+ /* read CONNACK */
 
- wifi.send( mqtt.getConnectMessage("testid"), strlen(conMsg));
- 
- wifi.releaseTCP();
-
-Serial.println("ENDE");
-
- if(exas) {
-  Serial.println("create tcp ok");
- } else {
-  Serial.println("create tcp failed");
- }
-
-
- 
- delay(5000);
-
-
-
-
+return true;
 
 }
+
+
+/* =======================================================================
+ *  disconnect from mqtt
+ * ======================================================================= */
+void mqttDisconnect() {
+  wifi.releaseTCP();
+}
+
 
 
 void dada() {
   
   uint8_t buffer[1024] = {0};
-  uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
+  uint32_t len = wifi.recv(buffer, sizeof(buffer), 5000);
+  
+
+  
+  
   if (len > 0) {
       Serial.print("Received:[");
       for(uint32_t i = 0; i < len; i++) {
-          Serial.print((char)buffer[i]);
+          //Serial.print((char)buffer[i]);
       }
       Serial.print("]\r\n");
   }
+
+  Serial.print("finsh write");
  
 }
 
