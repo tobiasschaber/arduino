82f360c639270e9d2fbb116cfbf315f0fa2488ea
Tobias Schaber <tobias.schaber@codecentric.de>
2016-07-12 08:07:22 +0200

    moved outputstream handling to handler thread

--- mqtt/sensors/mqtt-protocoltest/src/de/codecentric/mqtt/protocol/ReaderThread.java
+++ mqtt/sensors/mqtt-protocoltest/src/de/codecentric/mqtt/protocol/ReaderThread.java
@@ -1,18 +1,26 @@
 package de.codecentric.mqtt.protocol;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
  * Created by tobias on 08.07.16.
  */
-public class ReaderThread implements Runnable {
+public class ReaderThread extends Thread {
 
+    Socket socket;
     InputStream is;
+    OutputStream os;
 
 
-    public ReaderThread(InputStream is) {
-        this.is = is;
+    public ReaderThread(Socket socket) throws Exception {
+
+        this.socket = socket;
+        this.is = socket.getInputStream();
+        this.os = socket.getOutputStream();
 
     }
 
@@ -24,20 +32,21 @@ public class ReaderThread implements Runnable {
         int ct = 0;
         int read;
 
+        log("starte empfangen");
         try {
             while((is.read(buffer)) != -1) {
                 ++ct;
-                //System.out.println("reading:" + read);
             }
-            System.out.println("FINISHED READING " + ct + " bytes");
-            System.out.println("=========================================== PAYLOAD:");
-            System.out.println(new String(buffer, StandardCharsets.UTF_8));
-            System.out.print("=========================================== PAYLOAD BYTES:");
+
+            log("FINISHED READING " + ct + " bytes");
+            log("=========================================== PAYLOAD:");
+            log(new String(buffer, StandardCharsets.UTF_8));
+            log("=========================================== PAYLOAD BYTES:");
 
             for(int i=0;i<buffer.length;i++) {
                 if(buffer[i] != 0) {
                     if (i % 4 == 0) {
-                        System.out.println("");
+                        System.out.println(getName() + " : " + "");
                     }
 
                     System.out.print(String.format("%8s", Integer.toBinaryString(buffer[i])).replace(' ', '0')+ " | ");
@@ -46,12 +55,31 @@ public class ReaderThread implements Runnable {
                 }
             }
 
+            log("finished empfangen");
+
+
+            log("writing response");
+            byte[] response = "OK OK OK ".getBytes(Charset.forName("UTF-8"));
+            os.write(response);
+            //os.flush();
+
+
+            log("finished writing response");
+
+
+            Thread.sleep(4000);
+
+            log("closing socket");
+            socket.close();
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
 
-
+    public void log(String msg) {
+        System.out.println(getName() + " : " + msg);
     }
 }
